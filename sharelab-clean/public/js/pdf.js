f7d0ac8285bcf15c6ce2c601849dc0a5d3c2da5a
ronaldlb2024 <ronaldl_b@hotmail.com>
2025-08-17 15:14:36 -0300

    Delete sharelab-clean/public/js directory

--- sharelab-clean/public/js/pdf.js
+++
@@ -1,2 +0,0 @@
-// Placeholder for PDF.js
-var pdfjsLib = { getDocument: () => ({ promise: Promise.resolve({ numPages:1, getPage: async()=>({getTextContent: async()=>({items:[{str:'Exemplo PDF'}]})}) }) }) };

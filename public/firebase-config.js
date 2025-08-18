c4ce31fa3cb8b1c4dce29a0d1344f60e8e3156d0
ronaldlb2024 <ronaldl_b@hotmail.com>
2025-08-18 16:34:36 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,10 @@
-<script>
-  window.FIREBASE_CONFIG = {
-    apiKey: "REPLACE",
-    authDomain: "REPLACE.firebaseapp.com",
-    databaseURL: "https://REPLACE-default-rtdb.firebaseio.com",
-    projectId: "REPLACE",
-    storageBucket: "REPLACE.appspot.com",
-    messagingSenderId: "REPLACE",
-    appId: "REPLACE"
-  };
-</script>
-
+// public/firebase-config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};

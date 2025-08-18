16b1750c36c7885fe32a84d8cf2b4576eb77a5dc
ronaldlb2024 <ronaldl_b@hotmail.com>
2025-08-18 14:59:52 -0300

    Create firebase-config.example.js

---
+++ public/firebase-config.example.js
@@ -0,0 +1,13 @@
+<!-- public/firebase-config.example.js -->
+<script>
+  // COPIE ESTE ARQUIVO para public/firebase-config.js e preencha seus dados:
+  window.FIREBASE_CONFIG = {
+    apiKey: "REPLACE",
+    authDomain: "REPLACE.firebaseapp.com",
+    databaseURL: "https://REPLACE-default-rtdb.firebaseio.com",
+    projectId: "REPLACE",
+    storageBucket: "REPLACE.appspot.com",
+    messagingSenderId: "REPLACE",
+    appId: "REPLACE"
+  };
+</script>

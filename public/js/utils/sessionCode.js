e97a35c32b6b7bc6700d2ca981de01b09f9c1f5a
ronaldlb2024 <ronaldl_b@hotmail.com>
2025-08-23 09:28:56 -0300

    Update sessionCode.js

--- public/js/utils/sessionCode.js
+++ public/js/utils/sessionCode.js
@@ -1,6 +1,4 @@
-// public/js/utils/sessionCode.js
 export function newCode() {
-  // 4 dígitos, sem letras, evitando 0000/9999-like se quiser
   return String(Math.floor(1000 + Math.random() * 9000));
 }
 export function isValidCode(code) {

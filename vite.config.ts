33f2fec52e2267e9414d9a9e5cebc39e8b6ef6a5
ronaldlb2024 <ronaldl_b@hotmail.com>
2025-08-27 11:41:09 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/sharelab/', // ajuste se o nome do repo for outro
-  server: { port: 5173, open: false }
-});
